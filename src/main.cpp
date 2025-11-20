9ce6a2f905ade05a6071e2fd0737fbe3cdfdf97c
BrianGlasheen <pokemongamer223223@gmail.com>
2025-11-19 20:51:53 -0600

    compute shader

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,16 @@
 #include <cstdint>
 #include <vector>
 #include <deque>
+#include <span>
+#include <fstream>
 
-#define VK_CHECK(x)                                                     \
-    do {                                                                \
-        VkResult err = x;                                               \
-        if (err) {                                                      \
+#define VK_CHECK(x)                                                      \
+    do {                                                                 \
+        VkResult err = x;                                                \
+        if (err) {                                                       \
             printf("Detected Vulkan error: %s\n", string_VkResult(err)); \
-            abort();                                                    \
-        }                                                               \
+            abort();                                                     \
+        }                                                                \
     } while (0)
 
 bool validation_layers = true;
@@ -89,6 +91,93 @@ struct AllocatedImage {
 };
 AllocatedImage _drawImage;
 VkExtent2D _drawExtent;
+
+struct DescriptorLayoutBuilder {
+	std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+	void add_binding(uint32_t binding, VkDescriptorType type) {
+		VkDescriptorSetLayoutBinding newbind{};
+		newbind.binding = binding;
+		newbind.descriptorCount = 1;
+		newbind.descriptorType = type;
+
+		bindings.push_back(newbind);
+	}
+
+	void clear() {
+		bindings.clear();
+	}
+
+	VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext = nullptr, VkDescriptorSetLayoutCreateFlags flags = 0) {
+		for (auto& b : bindings) {
+			b.stageFlags |= shaderStages;
+		}
+
+		VkDescriptorSetLayoutCreateInfo info = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO };
+		info.pNext = pNext;
+
+		info.pBindings = bindings.data();
+		info.bindingCount = (uint32_t)bindings.size();
+		info.flags = flags;
+
+		VkDescriptorSetLayout set;
+		VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+
+		return set;
+	}
+};
+
+struct DescriptorAllocator {
+	struct PoolSizeRatio {
+		VkDescriptorType type;
+		float ratio;
+	};
+
+	VkDescriptorPool pool;
+
+	void init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios) {
+		std::vector<VkDescriptorPoolSize> poolSizes;
+		for (PoolSizeRatio ratio : poolRatios) {
+			poolSizes.push_back(VkDescriptorPoolSize{
+				.type = ratio.type,
+				.descriptorCount = uint32_t(ratio.ratio * maxSets)
+				});
+		}
+
+		VkDescriptorPoolCreateInfo pool_info = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO };
+		pool_info.flags = 0;
+		pool_info.maxSets = maxSets;
+		pool_info.poolSizeCount = (uint32_t)poolSizes.size();
+		pool_info.pPoolSizes = poolSizes.data();
+
+		vkCreateDescriptorPool(device, &pool_info, nullptr, &pool);
+	}
+
+	void clear_descriptors(VkDevice device) {
+		vkResetDescriptorPool(device, pool, 0);
+	}
+
+	void destroy_pool(VkDevice device) {
+		vkDestroyDescriptorPool(device, pool, nullptr);
+	}
+
+	VkDescriptorSet allocate(VkDevice device, VkDescriptorSetLayout layout) {
+		VkDescriptorSetAllocateInfo allocInfo = { .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO };
+		allocInfo.pNext = nullptr;
+		allocInfo.descriptorPool = pool;
+		allocInfo.descriptorSetCount = 1;
+		allocInfo.pSetLayouts = &layout;
+
+		VkDescriptorSet ds;
+		VK_CHECK(vkAllocateDescriptorSets(device, &allocInfo, &ds));
+
+		return ds;
+	}
+};
+
+DescriptorAllocator globalDescriptorAllocator;
+VkDescriptorSet _drawImageDescriptors;
+VkDescriptorSetLayout _drawImageDescriptorLayout;
 
 int init_vulkan(GLFWwindow *window) {
     vkb::InstanceBuilder builder;
@@ -459,6 +548,130 @@ void init_sync_structures() {
 	}
 }
 
+void init_descriptors() {
+	//create a descriptor pool that will hold 10 sets with 1 image each
+	std::vector<DescriptorAllocator::PoolSizeRatio> sizes = {
+		{ VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1 }
+	};
+
+	globalDescriptorAllocator.init_pool(_device, 10, sizes);
+
+	//make the descriptor set layout for our compute draw
+	DescriptorLayoutBuilder builder;
+	builder.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE);
+	_drawImageDescriptorLayout = builder.build(_device, VK_SHADER_STAGE_COMPUTE_BIT);
+
+	// other code
+	//allocate a descriptor set for our draw image
+	_drawImageDescriptors = globalDescriptorAllocator.allocate(_device, _drawImageDescriptorLayout);
+
+	VkDescriptorImageInfo imgInfo{};
+	imgInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+	imgInfo.imageView = _drawImage.imageView;
+
+	VkWriteDescriptorSet drawImageWrite = {};
+	drawImageWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	drawImageWrite.pNext = nullptr;
+
+	drawImageWrite.dstBinding = 0;
+	drawImageWrite.dstSet = _drawImageDescriptors;
+	drawImageWrite.descriptorCount = 1;
+	drawImageWrite.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+	drawImageWrite.pImageInfo = &imgInfo;
+
+	vkUpdateDescriptorSets(_device, 1, &drawImageWrite, 0, nullptr);
+
+	//make sure both the descriptor allocator and the new layout get cleaned up properly
+	_mainDeletionQueue.push_function([&]() {
+		globalDescriptorAllocator.destroy_pool(_device);
+		vkDestroyDescriptorSetLayout(_device, _drawImageDescriptorLayout, nullptr);
+	});
+}
+
+bool load_shader_module(const char* filePath, VkDevice device, VkShaderModule* outShaderModule) {
+	// open the file. With cursor at the end
+	std::ifstream file(filePath, std::ios::ate | std::ios::binary);
+
+	if (!file.is_open()) {
+		return false;
+	}
+
+	// find what the size of the file is by looking up the location of the cursor
+	// because the cursor is at the end, it gives the size directly in bytes
+	size_t fileSize = (size_t)file.tellg();
+
+	// spirv expects the buffer to be on uint32, so make sure to reserve a int
+	// vector big enough for the entire file
+	std::vector<uint32_t> buffer(fileSize / sizeof(uint32_t));
+
+	file.seekg(0);
+	file.read((char*)buffer.data(), fileSize);
+	file.close();
+
+	// create a new shader module, using the buffer we loaded
+	VkShaderModuleCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.pNext = nullptr;
+
+	// codeSize has to be in bytes, so multply the ints in the buffer by size of
+	// int to know the real size of the buffer
+	createInfo.codeSize = buffer.size() * sizeof(uint32_t);
+	createInfo.pCode = buffer.data();
+
+	// check that the creation goes well.
+	VkShaderModule shaderModule;
+	if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+		return false;
+	}
+	*outShaderModule = shaderModule;
+	return true;
+}
+
+VkPipeline _gradientPipeline;
+VkPipelineLayout _gradientPipelineLayout;
+
+void init_background_pipelines() {
+	VkPipelineLayoutCreateInfo computeLayout{};
+	computeLayout.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+	computeLayout.pNext = nullptr;
+	computeLayout.pSetLayouts = &_drawImageDescriptorLayout;
+	computeLayout.setLayoutCount = 1;
+
+	VK_CHECK(vkCreatePipelineLayout(_device, &computeLayout, nullptr, &_gradientPipelineLayout));
+
+	VkShaderModule computeDrawShader;
+	if (!load_shader_module("spirv/gradient.spv", _device, &computeDrawShader)) {
+		printf("Error when building the compute shader\n");
+		assert(false);
+	}
+
+	VkPipelineShaderStageCreateInfo stageinfo{};
+	stageinfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	stageinfo.pNext = nullptr;
+	stageinfo.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+	stageinfo.module = computeDrawShader;
+	stageinfo.pName = "main";
+
+	VkComputePipelineCreateInfo computePipelineCreateInfo{};
+	computePipelineCreateInfo.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+	computePipelineCreateInfo.pNext = nullptr;
+	computePipelineCreateInfo.layout = _gradientPipelineLayout;
+	computePipelineCreateInfo.stage = stageinfo;
+
+	VK_CHECK(vkCreateComputePipelines(_device, VK_NULL_HANDLE, 1, &computePipelineCreateInfo, nullptr, &_gradientPipeline));
+
+	vkDestroyShaderModule(_device, computeDrawShader, nullptr);
+
+	_mainDeletionQueue.push_function([&]() {
+		vkDestroyPipelineLayout(_device, _gradientPipelineLayout, nullptr);
+		vkDestroyPipeline(_device, _gradientPipeline, nullptr);
+	});
+}
+
+void init_pipelines() {
+	init_background_pipelines();
+}
+
 void cleanup() {
 	vkDeviceWaitIdle(_device);
 
@@ -490,14 +703,23 @@ void cleanup() {
 
 void draw_background(VkCommandBuffer cmd) {
 	//make a clear-color from frame number. This will flash with a 120 frame period.
-	VkClearColorValue clearValue;
-	float flash = std::abs(std::sin(_frameNumber / 120.f));
-	clearValue = { { 0.0f, 0.0f, flash, 1.0f } };
+	//VkClearColorValue clearValue;
+	//float flash = std::abs(std::sin(_frameNumber / 120.f));
+	//clearValue = { { 0.0f, 0.0f, flash, 1.0f } };
 
-	VkImageSubresourceRange clearRange = image_subresource_range(VK_IMAGE_ASPECT_COLOR_BIT);
+	//VkImageSubresourceRange clearRange = image_subresource_range(VK_IMAGE_ASPECT_COLOR_BIT);
 
 	//clear image
-	vkCmdClearColorImage(cmd, _drawImage.image, VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange);
+	//vkCmdClearColorImage(cmd, _drawImage.image, VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange);
+
+	// bind the gradient drawing compute pipeline
+	vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, _gradientPipeline);
+
+	// bind the descriptor set containing the draw image for the compute pipeline
+	vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, _gradientPipelineLayout, 0, 1, &_drawImageDescriptors, 0, nullptr);
+
+	// execute the compute pipeline dispatch. We are using 16x16 workgroup size so we need to divide by it
+	vkCmdDispatch(cmd, std::ceil(_drawExtent.width / 16.0), std::ceil(_drawExtent.height / 16.0), 1);
 }
 
 
@@ -518,6 +740,8 @@ int main() {
 	create_swapchain(width, height);
 	init_commands();
 	init_sync_structures();
+	init_descriptors();
+	init_pipelines();
 
     while (!glfwWindowShouldClose(window)) {
 		// wait until the gpu has finished rendering the last frame. Timeout of 1
